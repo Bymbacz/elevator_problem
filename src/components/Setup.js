72a9922ca914afd254c14aaf6cecb10a71db8fe2
Bymbacz <mateusz.bywalec2002@gmail.com>
2024-04-30 20:04:59 +0200

    Setup updated

--- src/components/Setup.js
+++ src/components/Setup.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import ElevatorSystem from './ElevatorSystem';
+import './Setup.css';
+
+class PersonInput extends React.Component {
+    render() {
+        return (
+            <div className="setup-div">
+                <label>
+                    Pickup floor:
+                    <input type="number" name="pickupFloor" min="0" max={this.props.floors - 1} value={this.props.pickupFloor} onChange={this.props.handleChange}/>
+                </label>
+                <label>
+                    Destination floor:
+                    <input type="number" name="destinationFloor" min="0" max={this.props.floors - 1} value={this.props.destinationFloor} onChange={this.props.handleChange}/>
+                </label>
+            </div>
+        );
+    }
+}
 
 class Setup extends React.Component {
     constructor(props) {
@@ -8,9 +26,7 @@ class Setup extends React.Component {
             elevators: 0,
             floors: 0,
             setupComplete: false,
-            people: [],
-            pickupFloor: 0,
-            destinationFloor: 0
+            people: [{ pickupFloor: 0, destinationFloor: 0 }],
         };
     }
 
@@ -21,48 +37,60 @@ class Setup extends React.Component {
     }
 
     // Function to handle the form input change
-    handleChange = (event) => {
-        this.setState({ [event.target.name]: parseInt(event.target.value) });
+    handleChange = (index, event) => {
+        const people = [...this.state.people];
+        people[index][event.target.name] = parseInt(event.target.value);
+        this.setState({ people });
     }
 
     // Function to handle the "Add person" button click
     handleAddPerson = () => {
         this.setState(prevState => ({
-            people: [...prevState.people, [prevState.pickupFloor, prevState.destinationFloor]]
+            people: [...prevState.people, { pickupFloor: 0, destinationFloor: 0 }]
         }));
+    }
+
+    // Function to check if all fields have values
+    isFormValid = () => {
+        return this.state.elevators > 0 && this.state.floors > 1 && this.state.people.every(person => person.pickupFloor >= 0 && person.destinationFloor >= 0);
     }
 
     // Render the ElevatorSystem component if the setup is complete, otherwise render the setup form
     render() {
         if (this.state.setupComplete) {
-            return <ElevatorSystem elevators={this.state.elevators} minFloor={0} maxFloor={this.state.floors-1} people={this.state.people} />;
+            return <ElevatorSystem elevators={this.state.elevators} minFloor={0} maxFloor={this.state.floors-1} people={this.state.people.map(person => [person.pickupFloor, person.destinationFloor])} />;
         } else {
             return (
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Number of elevators:
-                        <input type="number" name="elevators" min="0" max="16" onChange={this.handleChange}/>
-                    </label>
-                    <label>
-                        Number of floors:
-                        <input type="number" name="floors" min="1" max="10" onChange={this.handleChange}/>
-                    </label>
-                    <label>
-                        Pickup floor:
-                        <input type="number" name="pickupFloor" min="0" max={this.state.floors - 1}
-                               onChange={this.handleChange}/>
-                    </label>
-                    <label>
-                        Destination floor:
-                        <input type="number" name="destinationFloor" min="0" max={this.state.floors - 1}
-                               onChange={this.handleChange}/>
-                    </label>
-                    <button type="button" onClick={this.handleAddPerson}>Add person</button>
-                    <input type="submit" value="Submit"/>
-                </form>
-            );
+                <div className="setup">
+                    <form onSubmit={this.handleSubmit} className="setup-form">
+                        <h1>Elevator System Setup</h1>
+                        <div className="setup-div">
+                            <label>
+                                Number of elevators:
+                                <input type="number" name="elevators" min="0" max="16"
+                                       onChange={event => this.setState({elevators: parseInt(event.target.value)})}/>
+                            </label>
+                            <label>
+                                Number of floors:
+                                <input type="number" name="floors" min="1" max="10"
+                                       onChange={event => this.setState({floors: parseInt(event.target.value)})}/>
+                            </label>
+                        </div>
+                        {this.state.people.map((person, index) => (
+                            <PersonInput key={index} floors={this.state.floors} pickupFloor={person.pickupFloor}
+                                         destinationFloor={person.destinationFloor}
+                                         handleChange={event => this.handleChange(index, event)}/>
+                        ))}
+                        <br/>
+                        <div className="setup-div">
+                            <button type="button" onClick={this.handleAddPerson}>Add person</button>
+                            <input type="submit" value="Submit" disabled={!this.isFormValid()}/>
+                        </div>
+                    </form>
+                </div>
+        );
         }
-    }
+        }
 }
 
 export default Setup;
